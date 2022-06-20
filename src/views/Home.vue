3cb55476efa42bb625b5b9cef6669544d9cb436e
FilipK19 <101827622+FilipK19@users.noreply.github.com>
2022-06-20 23:03:00 +0200

    uređivanje prednje stranice

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,25 +3,35 @@
     <v-container class="grey lighten-5">
       <v-row no-gutters>
         <v-col>
-          <v-card width="344" height="300">
-            <v-img height="250" src="../assets/profile.svg"></v-img>
-            <router-link to="/profile">Profile</router-link>
-            <v-list-item-subtitle>Your profile</v-list-item-subtitle>
-          </v-card>
+          <v-btn width="344" height="300">
+            <router-link to="/profile">
+              <v-card width="344" height="300">
+                <v-img height="250" src="../assets/profile.svg"></v-img>
+                <p class="text-center">Profile</p>
+                <v-list-item-subtitle>Your profile</v-list-item-subtitle>
+              </v-card>
+            </router-link>
+          </v-btn>
         </v-col>
         <v-col>
-          <v-card max-width="344">
-            <v-img height="250" src="../assets/store.svg"></v-img>
-            <router-link to="/store">Store</router-link>
-            <v-list-item-subtitle>Buy stuff</v-list-item-subtitle>
-          </v-card>
+          <v-btn width="344" height="300">
+            <router-link to="/store">
+              <v-card width="344" height="300">
+                <v-img height="250" src="../assets/store.svg"></v-img>
+                <v-list-item-subtitle>Buy stuff</v-list-item-subtitle>
+              </v-card>
+            </router-link>
+          </v-btn>
         </v-col>
         <v-col>
-          <v-card max-width="344">
-            <v-img height="250" src="../assets/fish.svg"></v-img>
-            <router-link to="/fish">Fish</router-link>
-            <v-list-item-subtitle>Your fish</v-list-item-subtitle>
-          </v-card>
+          <v-btn width="344" height="300">
+            <router-link to="/store">
+              <v-card width="344" height="300">
+                <v-img height="250" src="../assets/fish.svg"></v-img>
+                <v-list-item-subtitle>Your fish</v-list-item-subtitle>
+              </v-card>
+            </router-link>
+          </v-btn>
         </v-col>
       </v-row>
     </v-container>
