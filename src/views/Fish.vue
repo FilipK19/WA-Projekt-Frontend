e06be428189ae542900b348e13de7875a9cc9a37
FilipK19 <101827622+FilipK19@users.noreply.github.com>
2022-06-23 02:46:16 +0200

    rad na random searchu

--- src/views/Fish.vue
+++ src/views/Fish.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="about">
-    hm:{{ imput }}
     <v-container class="grey lighten-5">
       <v-row no-gutters>
         <v-col>
@@ -17,7 +16,6 @@
             </v-btn>
           </router-link>
           <v-text-field label="Enter fish" v-model="input"> </v-text-field>
-          <v-btn color="#98FB98" @click="submit">Search</v-btn>
           <v-col> </v-col>
         </v-col>
         <v-col>
@@ -32,18 +30,29 @@
             >
           </v-btn>
           <v-col> </v-col>
+          <p>Random Number = {{ randomNumber }}</p>
+          <v-btn flat color="#98FB98" @click="myFunction()">Randomize</v-btn>
         </v-col>
         <v-col>
-          <v-btn rounded x-large width="900" height="100" color="#98FB98">
-            <v-img
-              height="60px"
-              width="60px"
-              src="../assets/random.svg"
-            ></v-img>
-            <v-list-item-subtitle style="color: #228b22"
-              >Learn about random fish</v-list-item-subtitle
+          <router-link :to="'/fish/FishDetails/' + rand">
+            <v-btn
+              @click="randomFish()"
+              rounded
+              x-large
+              width="900"
+              height="100"
+              color="#98FB98"
             >
-          </v-btn>
+              <v-img
+                height="60px"
+                width="60px"
+                src="../assets/random.svg"
+              ></v-img>
+              <v-list-item-subtitle style="color: #228b22"
+                >Learn about random fish</v-list-item-subtitle
+              >
+            </v-btn>
+          </router-link>
         </v-col>
       </v-row>
     </v-container>
@@ -58,11 +67,25 @@ export default {
   data() {
     return {
       input: "",
+      randomNumber: "",
+      rand: "",
     };
   },
   methods: {
     submit() {
       console.log(this.input);
+    },
+    myFunction: function () {
+      this.randomNumber = Math.random() * 10;
+    },
+    randomFish() {
+      if (this.randomNumber < 1) {
+        this.rand = "Pastrva";
+      } else if (this.randomNumber > 1 && this.randomNumber < 2) {
+        this.rand = "Srdela";
+      } else if (this.randomNumber > 2 && this.randomNumber < 3) {
+        this.rand = "Oslić";
+      }
     },
   },
 };
