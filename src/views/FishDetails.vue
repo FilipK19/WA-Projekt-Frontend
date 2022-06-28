77be2282e836dd0f408989b4810400eae4c0ef9b
FilipK19 <101827622+FilipK19@users.noreply.github.com>
2022-06-28 13:26:10 +0200

    detalji riba

--- src/views/FishDetails.vue
+++ src/views/FishDetails.vue
@@ -1,19 +1,150 @@
 <template>
   <div class="fish">
-    {{ fishData }}
-    <h2 v-for="f in fishData" v-bind:key="f">
-      <h2 v-if="fish == f.vrsta">
-        {{ f.vrsta + " " + f.voda }} {{ (i = i + 1) }}
-      </h2>
-    </h2>
-    <h4 v-if="i == 0">No {{ i }}</h4>
-    <h5 v-if="fish == fish1">
+    <v-card color="#98FB98">
+      <h3 style="color: #228b22" v-for="f in fishData" v-bind:key="f">
+        <h3 v-if="fish == f.vrsta">
+          {{ "Ime Ribe: " + f.vrsta }} <br />
+          {{ "Vrsta Ribe: " + f.voda }} <br />
+          {{ "Lokacije za pecat: " + f.lokacija }}
+        </h3>
+      </h3>
+    </v-card>
+    <h2 v-if="fish == 'Pastrva'">
       <v-img
-        height="225px"
-        width="450px"
+        height="380px"
+        width="1380px"
         src="../FishPictures/pastrva.jpg"
       ></v-img>
-    </h5>
+    </h2>
+    <h2 v-if="fish == 'Brancin'">
+      <v-img
+        height="390px"
+        width="1080px"
+        src="../FishPictures/brancin.png"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Orada'">
+      <v-img
+        height="400px"
+        width="680px"
+        src="../FishPictures/orada.png"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Zubatac'">
+      <v-img
+        height="390px"
+        width="960px"
+        src="../FishPictures/zubatac.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Cipal'">
+      <v-img
+        height="390px"
+        width="1260px"
+        src="../FishPictures/cipal.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Škrapina'">
+      <v-img
+        height="390px"
+        width="860px"
+        src="../FishPictures/škrapina.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Arbun'">
+      <v-img
+        height="390px"
+        width="1060px"
+        src="../FishPictures/arbun.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Trilja'">
+      <v-img
+        height="380px"
+        width="860px"
+        src="../FishPictures/trlja.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Ugor'">
+      <v-img
+        height="380px"
+        width="960px"
+        src="../FishPictures/ugor.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Morska Mačka'">
+      <v-img
+        height="290px"
+        width="1260px"
+        src="../FishPictures/morskamacka.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Skuša'">
+      <v-img
+        height="290px"
+        width="860px"
+        src="../FishPictures/skusa.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Tuna'">
+      <v-img
+        height="390px"
+        width="960px"
+        src="../FishPictures/tuna.png"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Špar'">
+      <v-img
+        height="390px"
+        width="860px"
+        src="../FishPictures/spar.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Šaran'">
+      <v-img
+        height="390px"
+        width="960px"
+        src="../FishPictures/saran.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Som'">
+      <v-img height="370px" width="960px" src="../FishPictures/som.jpg"></v-img>
+    </h2>
+    <h2 v-if="fish == 'Smuđ'">
+      <v-img
+        height="350px"
+        width="960px"
+        src="../FishPictures/smud.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Štuka'">
+      <v-img
+        height="390px"
+        width="1160px"
+        src="../FishPictures/stuka.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Kečiga'">
+      <v-img
+        height="350px"
+        width="960px"
+        src="../FishPictures/keciga.png"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Lipljen'">
+      <v-img
+        height="350px"
+        width="1160px"
+        src="../FishPictures/lipljen.jpg"
+      ></v-img>
+    </h2>
+    <h2 v-if="fish == 'Mladica'">
+      <v-img
+        height="370px"
+        width="1360px"
+        src="../FishPictures/mladica.jpg"
+      ></v-img>
+    </h2>
   </div>
 </template>
 
@@ -30,10 +161,8 @@ export default {
   data() {
     return {
       fishData: [],
-      fishyo: "",
-      i: -100,
-      fish1: "Pastrva",
-      fish2: "Srdela",
+      picture: "",
+      test1: "../FishPictures/pastrva.jpg",
     };
   },
 };
