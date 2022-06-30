3129c34497774c4e445c8b84397b1f03a5a938fe
FilipK19 <101827622+FilipK19@users.noreply.github.com>
2022-06-30 15:12:31 +0200

    ULJEPŠAVANJE

--- src/views/ProfileUpload.vue
+++ src/views/ProfileUpload.vue
@@ -1,20 +1,44 @@
 <template>
   <div>
-    <form @submit.prevent="createPost">
-      <div>
-        <label for="vrsta">Napišite ime upecane ribe </label>
-        <input type="text" id="vrsta" v-model="formData.vrsta" />
-      </div>
-      <div>
-        <label for="voda">Napišite vrstu upecane ribe </label>
-        <input type="text" id="voda" v-model="formData.voda" />
-      </div>
-      <div>
-        <label for="lokacija">Napišite gdje ste ju upecali </label>
-        <input type="text" id="lokacija" v-model="formData.lokacija" />
-      </div>
-      <button>Create Post</button>
-    </form>
+    <v-container>
+      <v-card style="color: #228b22" width="650" height="150" color="#98FB98">
+        <form @submit.prevent="createPost">
+          <h2>
+            <div>
+              <label for="vrsta">Napišite ime upecane ribe: </label>
+              <input
+                STYLE="background-color: #00FA9A;"
+                type="text"
+                id="vrsta"
+                v-model="formData.vrsta"
+              />
+            </div>
+            <v-divider class="mx-4"></v-divider>
+            <div>
+              <label for="voda">Napišite vrstu upecane ribe: </label>
+              <input
+                STYLE="background-color: #00FA9A;"
+                type="text"
+                id="voda"
+                v-model="formData.voda"
+              />
+            </div>
+            <v-divider class="mx-4"></v-divider>
+            <div>
+              <label for="lokacija">Napišite gdje ste ju upecali: </label>
+              <input
+                STYLE="background-color: #00FA9A;"
+                type="text"
+                id="lokacija"
+                v-model="formData.lokacija"
+              />
+            </div>
+            <v-divider class="mx-4"></v-divider>
+            <v-btn rounded color="#00FA9A">Create Post</v-btn>
+          </h2>
+        </form>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
